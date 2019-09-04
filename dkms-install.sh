f7d0d5e59798f66cf25de57e85dc9a62084cbaa5
Hamza Farooq <32929979+chilledHamza@users.noreply.github.com>
2019-09-04 14:30:20 +0500

    Fixed Installation Error.
    Workaround implemented for "Could not find module source directory"

---
+++ dkms-install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+  echo "You must run this with superuser priviliges.  Try \"sudo ./dkms-install.sh\"" 2>&1
+  exit 1
+else
+  echo "About to run dkms install steps..."
+fi
+
+DRV_DIR="$(pwd)"
+DRV_NAME=focaltech
+DRV_VERSION=2.0
+
+cp -r ${DRV_DIR} /usr/src/${DRV_NAME}-${DRV_VERSION}
+
+dkms add -m ${DRV_NAME} -v ${DRV_VERSION}
+dkms install -m ${DRV_NAME} -v ${DRV_VERSION}
+RESULT=$?
+
+echo "Finished running dkms install steps."
+
+modprobe -r psmouse && modprobe psmouse
+
+exit $RESULT
