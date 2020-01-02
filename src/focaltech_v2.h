6d2908c83601c8e87d82a5f5d537f9e8d4c1dc9f
Hamza <32929979+chilledHamza@users.noreply.github.com>
2020-01-02 20:01:35 +0500

    Update focaltech_v2.h

--- src/focaltech_v2.h
+++ src/focaltech_v2.h
@@ -1,29 +1,49 @@
 #ifndef _FOCALTECH_V2_H
 #define _FOCALTECH_V2_H
 
-#define FOC_MAX_FINGERS 4
+#define FOCALTECH_MAX_FINGERS 4
+
+/* 
+
+MAX_X reported by touchpad is 0x03E0 (992 decimal),
+With MAX_X set to 0x03E0 cursor movement is unnatural. 
+
+While MAX_X 0x0550 (1360 decimal) results in quite natural cursor movement,
+downside of using 0x0550 is that Horizontal Edge scrolling won't work.
+
+*/
+
+#define MAX_X 	0x08E0  /* 2272 */
+#define MAX_Y 	0x0550  /* 1360 */
+#define MAX_MAJOR 		10
+#define MAX_MINOR 		10
+#define MAX_PRESSURE 	127
+
+struct fte_command {
+	int command;
+	unsigned char data;
+};
 
 struct focaltech_finger_state
 {
     int x;
     int y;
-    int Width;
-    int Length;
-    bool active;
+    int major;
+    int minor;
+	int pressure;
+    bool valid;
 };
 
-
 struct focaltech_hw_state {
-	struct focaltech_finger_state fingers[FOC_MAX_FINGERS];
+	struct focaltech_finger_state fingers[FOCALTECH_MAX_FINGERS];
     int fingerCount;
-	bool leftPresseed;
-	bool rightPresseed;
+	bool left;
+	bool right;
 };
 
 struct focaltech_data {
 	bool isReadNext;
-	int infoData[16];
-	unsigned int x_max, y_max;
+	int lastDeviceData[16];
 	struct focaltech_hw_state state;
 };
 
